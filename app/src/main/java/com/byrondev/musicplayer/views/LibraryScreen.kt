5f8ed9c98394ab74c490fe44d9557804191f6f62
byron <1byron1999@gmail.com>
2024-12-18 14:20:03 -0800

    Add Horizontal divider

--- app/src/main/java/com/byrondev/musicplayer/views/LibraryScreen.kt
+++ app/src/main/java/com/byrondev/musicplayer/views/LibraryScreen.kt
@@ -3,38 +3,32 @@ package com.byrondev.musicplayer.views
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LeadingIconTab
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.byrondev.musicplayer.components.BottomBar
-import com.byrondev.musicplayer.components.albums.AlbumCard
 import com.byrondev.musicplayer.components.searchbar.SearchBar
 import com.byrondev.musicplayer.tabs.TabItem
 import com.byrondev.musicplayer.ui.theme.Gray20
-import com.byrondev.musicplayer.ui.theme.Gray30
 import com.byrondev.musicplayer.ui.theme.Pink60
 import com.byrondev.musicplayer.viewModels.MusicViewModels
 import kotlinx.coroutines.launch
@@ -67,8 +61,7 @@ fun LibraryScreenContent(
     val scope = rememberCoroutineScope()
     Column(modifier = Modifier.background(color=Color.Black).fillMaxSize().padding(paddingValues)) {
         SearchBar(navController)
-//        Spacer(modifier=Modifier.height(50.dp).fillMaxWidth())
-
+        Spacer(modifier=Modifier.height(10.dp).fillMaxWidth())
 
         TabRow (
             selectedTabIndex = pagerState.currentPage,
@@ -76,14 +69,14 @@ fun LibraryScreenContent(
             contentColor = Color.Transparent,
             indicator =  {},
             divider =  {
-//                HorizontalDivider(modifier = Modifier, color = Gray20, thickness = 1.dp)
+                HorizontalDivider( color = Gray20, thickness = 1.dp)
             }
             ) {
             tabs.forEachIndexed { index, tab ->
                 // OR Tab()
                 LeadingIconTab(
                     icon = { },
-                    text = { Text(tab.title) },
+                    text = { Text(tab.title, fontSize = 17.sp) },
                     selected = pagerState.currentPage == index,
                     selectedContentColor = Pink60,
                     unselectedContentColor = Color.White,
